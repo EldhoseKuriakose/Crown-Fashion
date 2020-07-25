e2508f3e7054374be2f8a816b359dd5e08a6b72e
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-07-25 14:49:39 +0530

    added checkout page, checkoutitem component, updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from './pages/ShopPage/ShopPage.component';
 import SignInAndSignUpPage from './pages/SignInAndSignUpPage/SignInAndSignUpPage.component';
+import CheckoutPage from './pages/CheckoutPage/CheckoutPage.component';
 import Header from './components/Header/Header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unSubscribeFromAuth = null; 
@@ -41,6 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -58,8 +62,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
