34db44c5ffef355ac6dfed108abb0dd18520a965
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-07-26 21:45:16 +0530

    edits

--- src/components/StripeCheckoutButton/StripeCheckoutButton.component.jsx
+++ src/components/StripeCheckoutButton/StripeCheckoutButton.component.jsx
@@ -12,7 +12,7 @@ const StripeCheckoutButton = ({ price }) => {
     return (
         <StripeCheckout
             label='Pay Now'
-            name='Crown Clothing'
+            name='Crown Fashion'
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/CUz.svg'
