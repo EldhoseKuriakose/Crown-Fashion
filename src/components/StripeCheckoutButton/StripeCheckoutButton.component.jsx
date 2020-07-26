4c29626746aec92229fa98f63eb231f53289d91a
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-07-26 21:23:22 +0530

    updating test stripe integration

---
+++ src/components/StripeCheckoutButton/StripeCheckoutButton.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51H9BVcLZN95zRORBcgYbwwvyeOQOiWXrgqiEcnsZpdfSMwl3osnhXHF7mEiwi6nHBaItkqEoCK9DlxyS2zuajxqI00ejO7AxQB';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crown Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
--- src/pages/CheckoutPage/CheckoutPage.component.jsx
+++ src/pages/CheckoutPage/CheckoutPage.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem.component';
+import StripeCheckoutButton from '../../components/StripeCheckoutButton/StripeCheckoutButton.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import './CheckoutPage.styles.scss';
 
@@ -30,6 +31,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments* 
+            <br />
+            4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
