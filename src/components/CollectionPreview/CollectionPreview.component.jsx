322ff6bcba9281d4ff2a03204d2bcea269af11cc
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-08-09 02:37:19 +0530

    edits

--- src/components/CollectionPreview/CollectionPreview.component.jsx
+++ src/components/CollectionPreview/CollectionPreview.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import CollectionItem from '../CollectionItem/CollectionItem.component';
 import './CollectionPreview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title' onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
                 .filter((item, idx) => idx  < 4)
@@ -16,4 +17,4 @@ const CollectionPreview = ({ title, items }) => (
     </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
